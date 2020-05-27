7560b8ca07b4bac17cdc0f6bc2de64b28141c351
gundogduelif <65432929+gundogduelif@users.noreply.github.com>
2020-05-27 13:49:18 -0400

    Assembled list of unique departments

--- groceries.py
+++ groceries.py
@@ -27,11 +27,13 @@ products = [
 ] # based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
 
 
-products = sorted(products, key=operator.itemgetter("name") )
-products_count = len(products)
-print("--------------")
-print("THERE ARE " + str(products_count) + " PRODUCTS:")
-print("--------------")
+# PART - 1 
+
+# products = sorted(products, key=operator.itemgetter("name") )
+# products_count = len(products)
+# print("--------------")
+# print("THERE ARE " + str(products_count) + " PRODUCTS:")
+# print("--------------")
 
 # {"id":1,
 #  "name": "Chocolate Sandwich Cookies", 
@@ -40,34 +42,49 @@ print("--------------")
 # "price": 3.50}
 # person["first_name"]
 
-for x in products:
-    usd = "${0:.2f}".format(x["price"])
-    print(" + " + x["name"] + " (" +str(usd) + ")")
+# for x in products:
+#    usd = "${0:.2f}".format(x["price"])
+#    print(" + " + x["name"] + " (" +str(usd) + ")")
 
 
 # --------------
 # THERE ARE 20 PRODUCTS:
 # --------------
-# + All-Seasons Salt ($4.99)
-# + Chocolate Fudge Layer Cake ($18.50)
-# + Chocolate Sandwich Cookies ($3.50)
-# + Cut Russet Potatoes Steam N' Mash ($4.25)
-# + Dry Nose Oil ($21.99)
-# + Fresh Scent Dishwasher Cleaner ($4.99)
-# + Gluten Free Quinoa Three Cheese & Mushroom Blend ($3.99)
-# + Green Chile Anytime Sauce ($7.99)
-# + Light Strawberry Blueberry Yogurt ($6.50)
-# + Mint Chocolate Flavored Syrup ($4.50)
-# + Overnight Diapers Size 6 ($25.50)
-# + Peach Mango Juice ($1.99)
-# + Pizza For One Suprema Frozen Pizza ($12.50)
-# + Pomegranate Cranberry & Aloe Vera Enrich Drink ($4.25)
-# + Pure Coconut Water With Orange ($3.50)
-# + Rendered Duck Fat ($9.99)
-# + Robust Golden Unsweetened Oolong Tea ($2.49)
-# + Saline Nasal Mist ($16.00)
-# + Smart Ones Classic Favorites Mini Rigatoni With Vodka Cream Sauce ($6.99)
-# + Sparkling Orange Juice & Prickly Pear Beverage ($2.99)
+ # + All-Seasons Salt ($4.99)
+ # + Chocolate Fudge Layer Cake ($18.50)
+ # + Chocolate Sandwich Cookies ($3.50)
+ # + Cut Russet Potatoes Steam N' Mash ($4.25)
+ # + Dry Nose Oil ($21.99)
+ # + Fresh Scent Dishwasher Cleaner ($4.99)
+ # + Gluten Free Quinoa Three Cheese & Mushroom Blend ($3.99)
+ # + Green Chile Anytime Sauce ($7.99)
+ # + Light Strawberry Blueberry Yogurt ($6.50)
+ # + Mint Chocolate Flavored Syrup ($4.50)
+ # + Overnight Diapers Size 6 ($25.50)
+ # + Peach Mango Juice ($1.99)
+ # + Pizza For One Suprema Frozen Pizza ($12.50)
+ # + Pomegranate Cranberry & Aloe Vera Enrich Drink ($4.25)
+ # + Pure Coconut Water With Orange ($3.50)
+ # + Rendered Duck Fat ($9.99)
+ # + Robust Golden Unsweetened Oolong Tea ($2.49)
+ # + Saline Nasal Mist ($16.00)
+ # + Smart Ones Classic Favorites Mini Rigatoni With Vodka Cream Sauce ($6.99)
+ # + Sparkling Orange Juice & Prickly Pear Beverage ($2.99)
+
+
+# PART - 2
+
+departments = []
+for x in products:
+    if x["departments"] not in departments:
+        departments.append(x["department"])
+
+department_count = len(departments)
+print("--------------")
+print("THERE ARE " + str(department_count) + " DEPARTMENTS:")
+print("--------------")
+
+
 # --------------
 # THERE ARE 10 DEPARTMENTS:
 # --------------
